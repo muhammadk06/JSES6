62689882c1062c138fa0024b9a0bf15c440783db
muhammadk06 <160337901+muhammadk06@users.noreply.github.com>
2024-10-12 15:57:25 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ let nam = ()=> {
 let name = document.getElementById("nm").value
 document.getElementById("mesge").innerHTML = "Have A Happy New Year " + name
 document.getElementById("pic").src = "d.jpeg"
+document.getElementById("bd").style.backgroundImage = "url(bg.gif)"
 }
 
 document.getElementById("cl").addEventListener("click", nam);
 
+let hal = () => {
+let name = document.getElementById("nm").value
+document.getElementById("mesge").innerHTML = "Have a Happy Halloween " + name
+document.getElementById("pic").src = "hal.jpeg"
+document.getElementById("bd").style.backgroundImage = "url(halloween.gif)"
+}
+document.getElementById("hal").addEventListener("click", hal);
